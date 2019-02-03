b034567dfd91bed9afc4e9289509b0b52fa46311
arahee001c <arahee001c@192.168.0.106>
2019-02-02 21:11:26 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# spring-caching-gemfire
+
+```
+"@springboot with @springframework is pure productivity! Who said in #java one has to write double the code than in other langs? #newFavLib"
+Cache Miss [true] - Elapsed Time [198 ms]
+"@springboot with @springframework is pure productivity! Who said in #java one has to write double the code than in other langs? #newFavLib"
+Cache Miss [false] - Elapsed Time [1 ms]
+"Really loving Spring Boot, makes stand alone Spring apps easy."
+Cache Miss [true] - Elapsed Time [24 ms]
+```
---
+++ src/main/java/hello/QuoteResponse.java
@@ -0,0 +1,40 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class QuoteResponse {
+
+	@JsonProperty("value")
+	private Quote quote;
+
+	@JsonProperty("type")
+	private String status;
+
+	@Override
+	public String toString() {
+		return String.format("{ @type = %1$s, quote = '%2$s', status = %3$s }", getClass().getName(), getQuote(),
+				getStatus());
+	}
+
+	public Quote getQuote() {
+		return quote;
+	}
+
+	public void setQuote(Quote quote) {
+		this.quote = quote;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
